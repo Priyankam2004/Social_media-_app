1163cf7da02ea04cf29f412f2e5cc6518f488d0a
Priyanka Magdum <magdumpriyanka7504@gmail.com>
2026-06-10 21:57:59 +0530

    Configure production CORS

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ if (!fs.existsSync(uploadsDir)) {
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'https://social-media-app-rosy-seven.vercel.app'],
   credentials: true
 }));
 
